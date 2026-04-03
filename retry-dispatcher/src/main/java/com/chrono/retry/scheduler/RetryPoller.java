9a716877c741ce7daccc6e3d481ae911815731a4
Shivang Garg <shivang10garg@gmail.com>
2026-04-03 15:59:03 +0530

    Refactor RetryPoller configuration: Update property names to use kebab-case in application.yml and adjust fixedDelay usage in RetryPoller class

--- retry-dispatcher/src/main/java/com/chrono/retry/scheduler/RetryPoller.java
+++ retry-dispatcher/src/main/java/com/chrono/retry/scheduler/RetryPoller.java
@@ -6,6 +6,8 @@ import com.chrono.retry.repository.RetryQueueRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,22 +16,28 @@ import java.util.List;
 @Slf4j
 @Component
 public class RetryPoller {
+
+    @Value("${retry.poller.batch-size:10}")
+    private int batchSize;
+
+    private final static String fixedDelay = "${retry.poller.fixed-delay:5000}";
+
     private final RetryQueueRepository retryQueueRepository;
     private final JobRequeueProducer jobRequeueProducer;
     private final ObjectMapper objectMapper;
 
     public RetryPoller(RetryQueueRepository retryQueueRepository,
-                       JobRequeueProducer jobRequeueProducer,
-                       ObjectMapper objectMapper) {
+            JobRequeueProducer jobRequeueProducer,
+            ObjectMapper objectMapper) {
         this.retryQueueRepository = retryQueueRepository;
         this.jobRequeueProducer = jobRequeueProducer;
         this.objectMapper = objectMapper;
         log.info("RetryPoller initialized successfully.");
     }
 
-    @Scheduled(fixedDelayString = "${retry.poller.fixedDelay:5000}")
+    @Scheduled(fixedDelayString = fixedDelay)
     public void pollAndDispatch() {
-        List<String> dueJobs = retryQueueRepository.fetchDueJobs(10, System.currentTimeMillis());
+        List<String> dueJobs = retryQueueRepository.fetchDueJobs(batchSize, System.currentTimeMillis());
 
         if (dueJobs.isEmpty()) {
             return;
